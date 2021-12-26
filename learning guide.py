33208eb579618085a764f51dd27a596cf0795ac5
marksikaundi <emmanuelmark117@gmail.com>
2021-12-26 22:35:52 +0200

    updated

---
+++ learning guide.py
@@ -0,0 +1,29 @@
+# # Learning Guide Unit 7
+
+
+# # Introduction
+# # In this unit, you will study two powerful data structures in Python, the dictionary and the tuple. You will learn how to use each of these types, along with how to use them together.
+
+# # Dictionaries (which are implemented as objects) provide a simple way to store data and retrieve it using an index key.  Dictionaries are based on key-value pairs. The key identifies the data and can be used to store, retrieve, and update the data. It must be immutable.
+
+# # The value part of the pair is the actual data, and it can be any type, including a list or even another dictionary. An example use of a dictionary might be to store the number of days in a month and retrieve it using the name of the month. For January, the key would be “January”, and the value would be 31.
+
+# # Tuples are sequences of values, like lists. Unlike lists and dictionaries, tuples are immutable. Thus tuples can be keys in a dictionary. Tuples are useful for assigning multiple values at once, for iterating through multiple lists at once, and for iterating through the key-value pairs in a dictionary.
+# Reading Assignment
+# Downey, A. (2015). Think Python: How to think like a computer scientist. This book is licensed under Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
+
+# Chapter 11 – Dictionaries
+# Chapter 12 - Tuples
+
+# Supplemental Reading
+
+# Downey, A. (2015). Think Python: How to think like a computer scientist. This book is licensed under Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0)
+
+# Chapter 13 - Case study: Data structure selection
+
+
+# Video Resources
+
+# kjdElectronics. (2017, August 5). Python beginner tutorial 8 - For loop, lists, and dictionaries [Video]. YouTube.
+
+
